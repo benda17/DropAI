9038c5f865a23a10e40062538501617d4b5ae21c
benda17 <110421851+benda17@users.noreply.github.com>
2025-07-15 20:39:56 +0300

    Create consts.py

---
+++ consts.py
@@ -0,0 +1,19 @@
+BEST_PRODUCTS_URL = "https://www.alura.io/best-selling-etsy-items"
+EXCEL_FILE = "/Users/benda/Desktop/DropAI/etsy_best_sellers.xlsx"
+ALI_LINKS_FILE = "/Users/benda/Desktop/DropAI/etsy_aliexpress_links.xlsx"
+SEARCH_URL   = "https://searchbyimage.com/?lang=en"
+SHEET_NAME = "Best Sellers"
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    )
+}
+
+INPUT_FILE   = "/Users/benda/Desktop/DropAI/etsy_best_sellers.xlsx"
+OUTPUT_FILE  = "/Users/benda/Desktop/DropAI/output_with_ali.xlsx"
+IMAGE_COL    = "image_url"
+RESULT_COL   = "ali_express_link"
+SEARCH_URL   = "https://searchbyimage.com/?lang=en"
+BASE_URL     = "https://searchbyimage.com"
